e389685c767f73b23afbf9fd2abad51f82324296
o_oyao <o_oyao@outlook.com>
2023-12-30 11:32:46 -0500

    add swagger ui to test delete.

--- service1/service_hosp/src/main/java/io/yao/harp/hosp/mapper/HospitalSetMapper.java
+++
@@ -1,7 +0,0 @@
-package io.yao.harp.hosp.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import io.yao.harp.model.hosp.HospitalSet;
-
-public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
-}
